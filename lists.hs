fb8d4da8a5067416b5aaab9d7ffa379cfcd0dada
AlanNunes <alann.625@gmail.com>
2018-07-17 13:03:51 -0300

    Recursivity Example

---
+++ lists.hs
@@ -0,0 +1,4 @@
+import Data.List
+import System.IO
+
+n1 = sum [1..10]
---
+++ recursivity.hs
@@ -0,0 +1,6 @@
+soma 1 = 1
+soma n = soma(n-1) + n
+
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci(n-1) + fibonacci(n-2)
